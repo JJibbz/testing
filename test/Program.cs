12a0da6808a7c329c9ee01671e2c3b60b5659f70
JJibbz <vkazancev322@gmail.com>
2024-05-17 23:04:07 +0700

    Добавьте файлы проекта.

---
+++ test/Program.cs
@@ -0,0 +1,39 @@
+﻿using AdoNetLib;
+using System.Data;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connector = new MainConnector();
+
+            var result = connector.ConnectAsync();
+
+            var data = new DataTable();
+
+            if (result.Result)
+            {
+                Console.WriteLine("Подключение успешно!");
+
+                var db = new DbExecutor(connector);
+
+                var tablename = "NetworkUser";
+
+                Console.WriteLine($"Получаем данные таблицы {tablename}");
+                
+                data = db.SelectAll(tablename);
+
+                Console.WriteLine("Отключаем БД!");
+                connector.DisconnectAsync();
+            }
+            else
+            {
+                Console.WriteLine("Ошибка подключения!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ testing/MainConnector.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using AdoNetLib.Configurations;
+
+namespace AdoNetLib
+{
+    public class MainConnector
+    {
+        private readonly SqlConnection connection = new SqlConnection(ConnectionString.MsSqlConnection);
+        public async Task <bool> ConnectAsync()
+        {
+            bool result;
+
+            try
+            { 
+                await connection.OpenAsync();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public async void DisconnectAsync()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                await connection.CloseAsync();
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                return connection;
+            }
+            else
+            {
+                throw new Exception("Подключение уже закрыто!");
+            }
+        }
+    }
+}
